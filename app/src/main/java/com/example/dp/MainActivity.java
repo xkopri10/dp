880c4d71d80f3229fb8245ba8e00bda30bfe7fe1
xkopri10 <xkopri10@node.mendelu.cz>
2019-08-23 17:51:54 +0200

    Addded: navigation bar, fragments

--- app/src/main/java/com/example/dp/MainActivity.java
+++ app/src/main/java/com/example/dp/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.dp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    MapFragment mapFragment = new MapFragment();
+    final StatisticsFragment statisticsFragment = new StatisticsFragment();
+    final MoreFragment moreFragment = new MoreFragment();
+    Fragment activeFragment = mapFragment;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_bar);
+        fragmentManager.beginTransaction().add(R.id.fragment_container, mapFragment, "3").commit();
+        fragmentManager.beginTransaction().add(R.id.fragment_container, statisticsFragment, "2").hide(statisticsFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.fragment_container, moreFragment, "1").hide(moreFragment).commit();
+
+        bottomNavigationView.setSelectedItemId(R.id.action_map);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.action_map:
+                        fragmentManager.beginTransaction().hide(activeFragment).show(mapFragment).commit();
+                        activeFragment = mapFragment;
+                        return true;
+                    case R.id.action_statistics:
+                        fragmentManager.beginTransaction().hide(activeFragment).show(statisticsFragment).commit();
+                        activeFragment = statisticsFragment;
+                        return true;
+                    case R.id.action_more:
+                        fragmentManager.beginTransaction().hide(activeFragment).show(moreFragment).commit();
+                        activeFragment = moreFragment;
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
